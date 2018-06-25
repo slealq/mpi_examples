8da77b4541c03fbaf71d8dda77db62516bc0d9f2
Stuart Leal Q <stuart.leal23@gmail.com>
2018-06-24 19:49:18 -0600

    Use int arrays instead of vector.

--- ej1_matrix_and_vectors/main.cpp
+++ ej1_matrix_and_vectors/main.cpp
@@ -4,11 +4,22 @@
 
 int main() {
   // Vector containing some integers
-  std::vector<int> v = {4, 12, 14, 34, 66};
+  int v[] = {12, 10, 7};
 
   // print the items
   for (int n : v) {
     std::cout << n << '\n';
   }
+
+  // Matrix with integers
+  int m[3][3] = {{2,4,3}, {1,5,7}, {0,2,3}};
+
+  // print the items
+  for (int i=0; i<3; i++) {
+    for (int n : m[i]) {
+      std::cout << n << '\n';
+    }
+  }
+
 
 }
