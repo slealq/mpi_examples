9188d4ec81fbe655c3e0550761d2167439ad6e0c
Stuart Leal Q <stuart.leal23@gmail.com>
2018-06-26 16:52:10 -0600

    Basic send and recv structure

--- ej1_matrix_and_vectors/parallel.cpp
+++ ej1_matrix_and_vectors/parallel.cpp
@@ -14,6 +14,23 @@ int main(int argc, char *argv[]) {
             << size
             << '\n';
 
+  if (rank == 0) {
+    // im the first rank
+    int number = 11;
+    MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    std::cout << "I'm rank "
+              << rank
+              << " sending a message to 1\n";
+  }
+
+  else if (rank == 1) {
+    int number;
+    MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    std::cout << "Process 1 received data : "
+              << number
+              << " \n";
+  }
+
   MPI_Finalize();
 
   return 0;
