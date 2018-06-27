f37710ac7ecef4d83e2ecabf326b740d56415b17
Stuart Leal Q <stuart.leal23@gmail.com>
2018-06-26 23:39:14 -0600

    Add while loop in slaves, and work signal

--- ej1_matrix_and_vectors/parallel.cpp
+++ ej1_matrix_and_vectors/parallel.cpp
@@ -8,6 +8,11 @@ int main(int argc, char *argv[]) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+  if (size < 2) {
+    std::cout << "More processes needed to run ...\n";
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
   if (rank == 0) {
     // this will be the master
 
@@ -26,6 +31,7 @@ int main(int argc, char *argv[]) {
     int n; // initial column to send
     int p = 1; // initial rank to send
     bool all=false; // boolean to account if all ranks have been used
+    int work=1; // work bool for slaves
     int received = 0; // count how many ranks have sent back their info
     int rpos; // rpos for the response position of the vector
     int result; // result from the other process
@@ -34,6 +40,7 @@ int main(int argc, char *argv[]) {
     for (n=0; n<n_total; n++) {
       if (!all) {
         // continue sending tasks until ranks are out
+        MPI_Send(&work, 1, MPI_INT, p, 10, MPI_COMM_WORLD); // tag 10 work bool
         MPI_Send(&n_total, 1, MPI_INT, p, 0, MPI_COMM_WORLD); // tag 0 for size of vector
         MPI_Send(&v, n_total, MPI_INT, p, 1, MPI_COMM_WORLD); // tag 1 will be v
         MPI_Send(&m[n], n_total, MPI_INT, p, 2, MPI_COMM_WORLD); // tag 2 will be m
@@ -63,6 +70,7 @@ int main(int argc, char *argv[]) {
         received ++;
 
         // since that process is now clean, send another tasks
+        MPI_Send(&work, 1, MPI_INT, p, 10, MPI_COMM_WORLD); // tag 10 work bool
         MPI_Send(&n_total, 1, MPI_INT, p, 0, MPI_COMM_WORLD); // tag 0 for size of vector
         MPI_Send(&v, n_total, MPI_INT, p, 1, MPI_COMM_WORLD); // tag 1 will be v
         MPI_Send(&m[n], n_total, MPI_INT, p, 2, MPI_COMM_WORLD); // tag 2 will be m
@@ -81,6 +89,12 @@ int main(int argc, char *argv[]) {
 
     } // n = received
 
+    // kill all slaves
+    work = 0; // set work to no
+    for (int i=0; i<size; i++) {
+      MPI_Send(&work, 1, MPI_INT, i, 10, MPI_COMM_WORLD); // send kill
+    }
+
     // print the result
     for (int n : r) {
       std::cout << n << "\n";
@@ -90,31 +104,45 @@ int main(int argc, char *argv[]) {
 
   else { // all slaves
 
-    int n_total;
-    int n;
-    int *v;
-    int *m;
-    int result=0;
+    while (1) {
 
-    MPI_Recv(&n_total, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      int n_total;
+      int n;
+      int *v;
+      int *m;
+      int result=0;
+      int work;
 
-    // allocate memory for the arrays
-    v = new int[n_total];
-    m = new int[n_total];
+      MPI_Recv(&work, 1, MPI_INT, 0, 10, MPI_COMM_WORLD, MPI_STATUS_IGNORE); // receive signal to work
+      if (work == 0) {
+        break; // escape from sequence
+      }
 
-    // receive the info
-    MPI_Recv(v, n_total, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    MPI_Recv(m, n_total, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    MPI_Recv(&n, 1, MPI_INT, 0, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(&n_total, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-    for (int i=0; i<n_total; i++) {
-      result += v[i] * m[i];
-    } // for int i
+      // allocate memory for the arrays
+      v = new int[n_total];
+      m = new int[n_total];
 
-    // return the result
-    MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-    MPI_Send(&n, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-    MPI_Send(&result, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+      // receive the info
+      MPI_Recv(v, n_total, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(m, n_total, MPI_INT, 0, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      MPI_Recv(&n, 1, MPI_INT, 0, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+      for (int i=0; i<n_total; i++) {
+        result += v[i] * m[i];
+      } // void name()or int i
+
+      // return the result
+      MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+      MPI_Send(&n, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+      MPI_Send(&result, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+
+      // free memory
+      delete v;
+      delete m;
+
+      }
 
   } // else
 
