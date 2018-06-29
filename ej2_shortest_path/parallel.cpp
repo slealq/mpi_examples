9be84730509da10ada50de7a9ce5a102958e7993
Stuart Leal Q <stuart.leal23@gmail.com>
2018-06-28 22:37:35 -0600

    Add basic structure for a parallel program

--- ej2_shortest_path/parallel.cpp
+++ ej2_shortest_path/parallel.cpp
@@ -1,5 +1,7 @@
 #include <mpi.h>
 #include <iostream>
+#include <algorithm>
+#include <climits>
 #include "parallel.h"
 
 int main(int argc, char *argv[]) {
@@ -15,6 +17,8 @@ int main(int argc, char *argv[]) {
   }
 
   if (rank==0) {
+    // master rank
+
     // len of the cities graph
     int len = 8;
 
@@ -40,6 +44,90 @@ int main(int argc, char *argv[]) {
                              {26, 92, 34, 43, 193, 0, 108, 55},
                              {83, 197, 78, 69, 98, 108, 0, 141},
                              {75, 100, 83, 91, 236, 55, 141, 0} };
+
+     // options of cities, to permutate
+  int options[len-1];
+
+  // number of city to fill in options
+  int n=0; // start with 0
+
+  // fill the options
+  for (int i=0; i<len; i++) {
+    if (i!=start) {
+      // while n is different from start city
+      options[n] = i;
+
+      // increase n
+      n++;
+    } // if
+  } // for
+
+  // set the minimum path to the max int len
+  int min_path = INT_MAX;
+  int min_option[len-1];
+
+  // define variables for the loop
+  int current_city;
+  int current_pathweight;
+
+  // variables for the master
+  int p=1; // rank to send code to
+  bool all=false; // all ranks have been used for the first times
+  int work=1; // work for slaves
+  int received=0; // count how many ranks have sent back their info
+
+  // while there is not permuatation left
+  do {
+    if (!all) {
+      // continue sending tasks until ranks are out
+
+      // increment p
+      p++;
+
+      // if there are no more ranks left
+      if (p==size) {
+        // raise all flag
+        all = true;
+
+      } // if p == size
+    } // if not all
+
+    else {
+      // all ranks have been used
+      // receive rank from any process
+
+      // increase received
+      received ++;
+
+      // since that process is now clean, send another tasks
+    } // else
+
+
+  } while (std::next_permutation(options, options+len-1));
+
+  // receive the process that are left
+
+  // kill all slaves
+
+  // print the result
+
   } // rank 0 code
+
+  else { // all slaves
+
+    while (1) {
+
+      // wait for work signal or kill signal
+
+      // do work
+
+      // send back result
+
+    } // while ever
+
+  } // else
+
+  MPI_Finalize();
+  return 0;
 
 } // main
