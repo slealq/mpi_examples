f8afb6411e954d42b31e70678d921c32f1a1d8a1
Stuart Leal Q <stuart.leal23@gmail.com>
2018-06-28 19:06:19 -0600

    Review code bug

--- ej3_primes/ej3_primes_par.cpp
+++ ej3_primes/ej3_primes_par.cpp
@@ -20,13 +20,13 @@ using namespace std;
  * Main driver
 */
 int main(int argc, char* argv[]){
-	int startT = clock();
+    int startT = clock();
     // Initialize MPI vars
     int rank, size, x, r;
     MPI_Status status;
     MPI_Init(&argc , &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size); 
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     int limit = atoi(argv[1]);
     // Array of ranks to determine who is busy. 1 busy and 0 otherwise
     bool busy_rank[size];
@@ -60,7 +60,7 @@ int main(int argc, char* argv[]){
             work = false;
         }
         else {
-            MPI_Recv(sieveA,limit,MPI_INT,0,MSG_ARRAY,MPI_COMM_WORLD,&status);       
+            MPI_Recv(sieveA,limit,MPI_INT,0,MSG_ARRAY,MPI_COMM_WORLD,&status);
             MPI_Recv(&x,limit,MPI_INT,0,MSG_01,MPI_COMM_WORLD,&status);
             MPI_Recv(busy_rank, size, MPI_INT, 0, MSG_BOOL, MPI_COMM_WORLD,&status);
             /* Description of algorithm:
@@ -69,9 +69,9 @@ int main(int argc, char* argv[]){
                     odd number of distinct pairs (x, y) that satisfy the equation and
                     n module 12 = 1 or n module 12 = 5.
                 b) n = (3*x^2)+(y^2) has odd number of solutions and n module 12 = 7
-                c) n = (3*x*x)-(y^2) has odd number of solutions, where x > y and 
-                    n module 12 = 11 
-                Source: https://en.wikipedia.org/wiki/Sieve_of_Atkin 
+                c) n = (3*x*x)-(y^2) has odd number of solutions, where x > y and
+                    n module 12 = 11
+                Source: https://en.wikipedia.org/wiki/Sieve_of_Atkin
             */
             for (int y = 1; y*y < limit; y++) {
                 // Main part of Sieve of Atkin
@@ -88,8 +88,8 @@ int main(int argc, char* argv[]){
                     sieveA[n] ^= true;
                 }
             }
-            busy_rank[rank]=false;        
-        }   
+            busy_rank[rank]=false;
+        }
     }
     work = true;
     while (work){
@@ -107,20 +107,20 @@ int main(int argc, char* argv[]){
                 busy_rank[k]=true;
                 MPI_Send(sieveA, limit, MPI_INT, k, MSG_ARRAY, MPI_COMM_WORLD);
                 MPI_Send(&r, 1, MPI_INT, k, MSG_01, MPI_COMM_WORLD);
-                MPI_Send(busy_rank, size, MPI_INT, k, MSG_BOOL, MPI_COMM_WORLD);       
-            }   
+                MPI_Send(busy_rank, size, MPI_INT, k, MSG_BOOL, MPI_COMM_WORLD);
+            }
             work = false;
         }
         else {
-            MPI_Recv(sieveA,limit,MPI_INT,0,MSG_ARRAY,MPI_COMM_WORLD,&status);       
+            MPI_Recv(sieveA,limit,MPI_INT,0,MSG_ARRAY,MPI_COMM_WORLD,&status);
             MPI_Recv(&r,limit,MPI_INT,0,MSG_01,MPI_COMM_WORLD,&status);
             MPI_Recv(busy_rank, size, MPI_INT, 0, MSG_BOOL, MPI_COMM_WORLD,&status);
             if (sieveA[r]) {
                 for (int i = r*r; i < limit; i += r*r)
                     sieveA[i] = false;
             }
-            busy_rank[rank]=false;        
-        }   
+            busy_rank[rank]=false;
+        }
     }
     if (rank==0){
         // Print primes using sieveA[]
@@ -133,9 +133,9 @@ int main(int argc, char* argv[]){
         if (limit > 2)
             std::cout << 2 << " ";
         if (limit > 3)
-            std::cout << 3 << " ";        
-        std::cout << endl << "Primes: " << cont; 
-        std::cout << endl << "Execution Time: " << (double)(clock()-startT)/CLOCKS_PER_SEC << "s" << endl;        
+            std::cout << 3 << " ";
+        std::cout << endl << "Primes: " << cont;
+        std::cout << endl << "Execution Time: " << (double)(clock()-startT)/CLOCKS_PER_SEC << "s" << endl;
     }
-	return 0;
+    return 0;
 }
